ebbac614bd69a50827fa6f4e4914305ced65d6ce
Frankie Leyva <frankieiii15@comcast.net>
2024-03-13 17:33:35 -0700

    Pages Update

--- components/PlanPage.tsx
+++ components/PlanPage.tsx
@@ -1,9 +1,31 @@
 import styles from '@/styles/Home.module.css'
+import font from '@/components/fonts'
+import strategylogo from '@/public/images/icons/plan-icon.png'
+import designlogo from '@/public/images/icons/design-icon.png'
+import refinelogo from '@/public/images/icons/refine-icon.png'
 
 const PlanPage = ({}: {}) => {
   return (
-    <div className={styles.fullpage2}>
-
+    <div className={`${styles.fullpagesolid} ${font.className}`}>
+      <div className={styles.plantitle}>How it works</div>
+      <div className={styles.placontainer}>
+        <div className={styles.plansection}>
+          <h2 className={styles.planheading}>Strategy</h2>
+          <img className={styles.planicon} src={strategylogo.src}/>
+          <p className={styles.plansubtext}>We start off with a discussion regarding your goals and desired features. The better we know your objectives the faster we can connect your audience to your services.</p>
+        </div>
+        <div className={styles.plansection}>
+          <h2 className={styles.planheading}>Design</h2>
+          <img className={styles.planicon} src={designlogo.src}/>
+          <p className={styles.plansubtext}>Next, we follow a 3 step building process of creating a Wireframe, landing page, and full site. We provide deliverables at each step, accepting feedback and revisions to ensure complete satisfaction.</p>
+        </div>
+        <div className={styles.plansection}>
+          <h2 className={styles.planheading}>Refine</h2>
+          <img className={styles.planicon} src={refinelogo.src}/>
+          <p className={styles.plansubtext}>The finished website will be completely launched for you. You will have full technical support and the opportunity for final revisions for up to 30 days following the date of completion.</p>
+        </div>
+      </div>
+      <button className={styles.ctabutton}>Get Started</button>
     </div>
   );
 };
--- components/ServicesPage.tsx
+++ components/ServicesPage.tsx
@@ -3,7 +3,36 @@ import styles from '@/styles/Home.module.css'
 const ServicesPage = ({}: {}) => {
   return (
     <div className={styles.fullpage2}>
-
+      <div>
+        <div>
+          <h2>Flex</h2>
+          <p>Starting at 500</p>
+          <p>One time price</p>
+          <div>
+            Strategy discussion addressing your goals
+            Custom eye-catching website design using modern visuals
+            Handling of website launch at your desired domain.
+            All website code provided along with set up host accounts to offer flexibility in future changes.
+            3 comprehensive revisions applicable for any part of the design process
+            30 days technical support and additional revision period
+          </div>
+          <p>*Optimal for established businesses with solidified services and long term goals.</p>
+        </div>
+        <div>
+          <h2>Managed</h2>
+          <p>Starting at 300</p>
+          <p> With 12/Mo subscription</p>
+          <div>
+            Strategy discussion addressing your goals
+            Custom eye-catching website design using modern visuals
+            Full handling of website launch  at your desired domain as well as continued maintenance.
+            3 comprehensive revisions applicable for any part of the design process
+            Continued website support.
+            Ability to make 1 change per month to update services, offerings, or information.
+          </div>
+          <p>*Optimal for small businesses just starting out and iterating upon their services.</p>
+        </div>
+      </div>
     </div>
   );
 };
