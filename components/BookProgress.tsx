efdb8c9a3c52d74fdeccb274957fb722bdaa15ef
Frankie Leyva <frankieiii15@comcast.net>
2024-04-06 09:45:08 -0700

    Add step headings

--- components/BookProgress.tsx
+++ components/BookProgress.tsx
@@ -10,7 +10,7 @@ const BookProgress = ({progress}: {progress: number}) => {
 
   const steps = ['', '', '', ''];
   const [activeStep, setActiveStep] = useState(0);
-  const [userinfo, setInfo] = useState([]);
+  const [userinfo, setInfo] = useState<Array<string>>([]);
 
   return (
     <div className={`${styles.fullpage} ${font.className}`}>
@@ -29,7 +29,9 @@ const BookProgress = ({progress}: {progress: number}) => {
         <h1 className={styles.primarytext}>Let's get started!</h1>
         <h3 className={styles.secondarytext}> Do you have a website plan in mind? </h3>
         <div className={styles.actioninterface}> 
-        
+          <button onClick={() => {setInfo(["Flex"]); setActiveStep(1)}}> Flex </button>
+          <button onClick={() => {setInfo(["Managed"]); setActiveStep(1)}}> Managed </button>
+          <button onClick={() => {setInfo(["Undecided"]); setActiveStep(1)}}> I'm not sure </button>
         </div>
       </div>}
       {activeStep == 1 && <div className={styles.centercontainer}>
@@ -44,10 +46,10 @@ const BookProgress = ({progress}: {progress: number}) => {
             })}
           </Stepper>
         </div>
-        <h1 className={styles.primarytext}>Let's get started!</h1>
-        <h3 className={styles.secondarytext}> Do you have a website plan in mind? </h3>
+        <h1 className={styles.primarytext}>{userinfo[0] == "Undecided" ? "No probelm," : "Great, "}</h1>
+        <h3 className={styles.secondarytext}> Let's get your contact information. </h3>
         <div className={styles.actioninterface}> 
-        
+          <button onClick={() => { ;setActiveStep(2)}}> Next </button>
         </div>
       </div>}
       {activeStep == 2 && <div className={styles.centercontainer}>
@@ -62,10 +64,10 @@ const BookProgress = ({progress}: {progress: number}) => {
             })}
           </Stepper>
         </div>
-        <h1 className={styles.primarytext}>Let's get started!</h1>
-        <h3 className={styles.secondarytext}> Do you have a website plan in mind? </h3>
+        <h1 className={styles.primarytext}> Thank you, </h1>
+        <h3 className={styles.secondarytext}> Now about your business and website... </h3>
         <div className={styles.actioninterface}> 
-        
+          <button onClick={() => { ;setActiveStep(3)}}> Next </button>
         </div>
       </div>}
       {activeStep == 3 && <div className={styles.centercontainer}>
@@ -80,10 +82,10 @@ const BookProgress = ({progress}: {progress: number}) => {
             })}
           </Stepper>
         </div>
-        <h1 className={styles.primarytext}>Let's get started!</h1>
-        <h3 className={styles.secondarytext}> Do you have a website plan in mind? </h3>
+        <h1 className={styles.primarytext}>Perfect!</h1>
+        <h3 className={styles.secondarytext}> Before submitting review your infomation below. </h3>
         <div className={styles.actioninterface}> 
-        
+          <button onClick={() => { ;setActiveStep(4)}}> Next </button>
         </div>
       </div>}
       {activeStep == 4 && <div className={styles.centercontainer}>
@@ -98,11 +100,9 @@ const BookProgress = ({progress}: {progress: number}) => {
             })}
           </Stepper>
         </div>
-        <h1 className={styles.primarytext}>Let's get started!</h1>
-        <h3 className={styles.secondarytext}> Do you have a website plan in mind? </h3>
-        <div className={styles.actioninterface}> 
-        
-        </div>
+        <h1 className={styles.primarytext}>Submission recieved!</h1>
+
+
       </div>}
     </div>
   );
