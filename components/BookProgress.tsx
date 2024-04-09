0e075de1174ca1d9f560461415a490ecdd9f6da1
Frankie Leyva <frankieiii15@comcast.net>
2024-04-09 09:56:51 -0700

    Refine application process

--- components/BookProgress.tsx
+++ components/BookProgress.tsx
@@ -1,6 +1,7 @@
 import styles from '@/styles/Start.module.css'
 import font from '@/components/fonts'
 import {useState} from 'react';
+import axios from 'axios';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
@@ -11,29 +12,42 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import Button from '@mui/material/Button';
+import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
+import ExpandCircleDownOutlinedIcon from '@mui/icons-material/ExpandCircleDownOutlined';
 
 
 const BookProgress = ({progress}: {progress: number}) => {
 
   const steps = ['', '', '', ''];
-  const [activeStep, setActiveStep] = useState(0);
-  const [userinfo, setInfo] = useState<Array<string>>([]);
-  const [dataName, setDataName] = useState("");
-  const [dataNumber, setDataNumber] = useState("");
-  const [dataEmail, setDataEmail] = useState("");
-  const [dataPref, setDataPref] = useState("");
-  const [dataType, setDataType] = useState("");
-  const [dataIndustry, setDataIndustry] = useState("");
-  const [dataAdditional, setDataAdditional] = useState("");
+  const [activeStep, setActiveStep] = useState<number>(0);
+  const [dataPlan, setDataPlan] = useState<string | undefined>();
+  const [dataName, setDataName] = useState<string | undefined>();
+  const [dataNumber, setDataNumber] = useState<string | undefined>();
+  const [dataEmail, setDataEmail] = useState<string | undefined>();
+  const [dataPref, setDataPref] = useState<string | undefined>();
+  const [dataType, setDataType] = useState<string | undefined>();
+  const [dataIndustry, setDataIndustry] = useState<string | undefined>();
+  const [dataAdditional, setDataAdditional] = useState<string | undefined>();
+  
+  async function submitData() {
+    try {
+      const response = await axios.get(`/api/emailnoti?name=${dataName}&number=${dataNumber}&email=${dataEmail}&pref=${dataPref}&plan=${dataPlan}&type=${dataType}&industry=${dataIndustry}${dataAdditional ? `&additional=${dataAdditional}` :``}`);
+      console.log(response);
+      if(response){setActiveStep(4)}
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <div className={`${styles.fullpage} ${font.className}`}>
+      <ExpandCircleDownOutlinedIcon onClick={() => {setActiveStep(activeStep-1)}} className={styles.backicon} style={{display: (activeStep > 0 && activeStep < 4)? "" : "none"}} />
       {activeStep == 0 && <div className={styles.centercontainer}>
         <div className={styles.stepcontainer}>
           <Stepper activeStep={activeStep}>
             {steps.map((label, index) => {
               return (
-                <Step key={label} >
+                <Step key={index} >
                   <StepLabel>{label}</StepLabel>
                 </Step>
               );
@@ -43,9 +57,9 @@ const BookProgress = ({progress}: {progress: number}) => {
         <h1 className={styles.primarytext}>Let's get started!</h1>
         <h3 className={styles.secondarytext}> Do you have a website plan in mind? </h3>
         <div className={styles.actioninterface}> 
-          <button onClick={() => {setInfo(["Flex"]); setActiveStep(1)}} className={styles.planbutton}> <div className={styles.flexrow}><h2 className={styles.planmaintext}>Flex</h2><p className={styles.planaddtext}>╺Starting at 350 with $15/mo subscription</p></div> <div className={styles.planpropertiescontainer}><h4 className={styles.planprop}>● Strategy discussion addressing goals.</h4><h4 className={styles.planprop}>● Website launch at your desired domain.</h4><h4 className={styles.planprop}>● A change per month to update services.</h4><h4 className={styles.planprop}>● Continuous website support.</h4></div> </button>
-          <button onClick={() => {setInfo(["Managed"]); setActiveStep(1)}} className={styles.planbutton}> <div className={styles.flexrow}><h2 className={styles.planmaintext}>Managed</h2><p className={styles.planaddtext}>╺Starting at 500</p></div> <div className={styles.planpropertiescontainer}><h4 className={styles.planprop}>● Strategy discussion addressing goals.</h4><h4 className={styles.planprop}>● Website launch at your desired domain.</h4><h4 className={styles.planprop}>● Set up host accounts.</h4><h4 className={styles.planprop}>● All website code provided.</h4></div> </button>
-          <button onClick={() => {setInfo(["Undecided"]); setActiveStep(1)}} className={styles.planbuttonalt}> <div className={styles.flexrow}><h4 className={styles.planalttext}>I'm not sure.</h4></div> </button>
+          <button onClick={() => {setDataPlan("Flex"); setActiveStep(1)}} className={styles.planbutton}> <div className={styles.flexrow}><h2 className={styles.planmaintext}>Flex</h2><p className={styles.planaddtext}>╺Starting at 350 with $15/mo subscription</p></div> <div className={styles.planpropertiescontainer}><h4 className={styles.planprop}>● Strategy discussion addressing goals.</h4><h4 className={styles.planprop}>● Website launch at your desired domain.</h4><h4 className={styles.planprop}>● A change each month to update services.</h4><h4 className={styles.planprop}>● Continuous website support.</h4></div> </button>
+          <button onClick={() => {setDataPlan("Managed"); setActiveStep(1)}} className={styles.planbutton}> <div className={styles.flexrow}><h2 className={styles.planmaintext}>Managed</h2><p className={styles.planaddtext}>╺Starting at 500</p></div> <div className={styles.planpropertiescontainer}><h4 className={styles.planprop}>● Strategy discussion addressing goals.</h4><h4 className={styles.planprop}>● Website launch at your desired domain.</h4><h4 className={styles.planprop}>● Set up host accounts.</h4><h4 className={styles.planprop}>● All website code provided.</h4></div> </button>
+          <button onClick={() => {setDataPlan("Undecided"); setActiveStep(1)}} className={styles.planbuttonalt}> <div className={styles.flexrow}><h4 className={styles.planalttext}>I'm not sure.</h4></div> </button>
         </div>
       </div>}
       {activeStep == 1 && <div className={styles.centercontainer}>
@@ -53,14 +67,14 @@ const BookProgress = ({progress}: {progress: number}) => {
           <Stepper activeStep={activeStep}>
             {steps.map((label, index) => {
               return (
-                <Step key={label} >
+                <Step key={index} >
                   <StepLabel>{label}</StepLabel>
                 </Step>
               );
             })}
           </Stepper>
         </div>
-        <h1 className={styles.primarytext}>{userinfo[0] == "Undecided" ? "No probelm," : `Great, we will discuss a ${userinfo[0]} plan.`}</h1>
+        <h1 className={styles.primarytext}>{dataPlan == "Undecided" ? "No probelm," : `Great, we will discuss a ${dataPlan} plan.`}</h1>
         <h3 className={styles.secondarytext}> Let's get your contact information. </h3>
         <div className={styles.actioninterface}> 
           <TextField onChange={(event) => {setDataName(event.target.value)}} id="outlined-size-small" label="Name" size="small" style={{width: '50%', marginBlock: "10px"}} />
@@ -73,7 +87,7 @@ const BookProgress = ({progress}: {progress: number}) => {
               <FormControlLabel value="Email" control={<Radio />} label="Email" />
             </RadioGroup>
           </FormControl>
-          <Button onClick={() => { ;setActiveStep(2)}} className={styles.next} variant="outlined">Next</Button>
+          <Button onClick={() => { ;setActiveStep(2)}} className={styles.next} disabled={(dataName && dataEmail && dataNumber && dataPref)? false : true} variant="outlined">Next</Button>
         </div>
       </div>}
       {activeStep == 2 && <div className={styles.centercontainer}>
@@ -81,7 +95,7 @@ const BookProgress = ({progress}: {progress: number}) => {
           <Stepper activeStep={activeStep}>
             {steps.map((label, index) => {
               return (
-                <Step key={label} >
+                <Step key={index} >
                   <StepLabel>{label}</StepLabel>
                 </Step>
               );
@@ -100,7 +114,7 @@ const BookProgress = ({progress}: {progress: number}) => {
           <TextField onChange={(event) => {setDataIndustry(event.target.value)}} id="outlined-size-small" label="Industry" size="small" style={{width: '50%', marginBlock: "10px"}} />
           <h4>Anything else we should know?</h4>
           <TextField onChange={(event) => {setDataAdditional(event.target.value)}} id="outlined-size-small" multiline rows={3} label="Additional info" size="small" style={{width: '70%', marginBlock: "10px"}} />
-          <Button onClick={() => { ;setActiveStep(3)}} className={styles.next} variant="outlined">Next</Button>
+          <Button onClick={() => { ;setActiveStep(3)}} className={styles.next} disabled={(dataType && dataIndustry)? false : true} variant="outlined">Next</Button>
         </div>
       </div>}
       {activeStep == 3 && <div className={styles.centercontainer}>
@@ -108,7 +122,7 @@ const BookProgress = ({progress}: {progress: number}) => {
           <Stepper activeStep={activeStep}>
             {steps.map((label, index) => {
               return (
-                <Step key={label} >
+                <Step key={index} >
                   <StepLabel>{label}</StepLabel>
                 </Step>
               );
@@ -118,14 +132,14 @@ const BookProgress = ({progress}: {progress: number}) => {
         <h1 className={styles.primarytext}>Perfect!</h1>
         <h3 className={styles.secondarytext}> Before submitting, review your infomation below. </h3>
         <div className={styles.actioninterface}> 
-          <div className={styles.datatext}><h4>Name:</h4><p> {dataName} </p></div>
-          <div className={styles.datatext}><h4>Number:</h4><p> {dataNumber} </p></div>
-          <div className={styles.datatext}><h4>Email:</h4><p> {dataEmail} </p></div>
-          <div className={styles.datatext}><h4>Preferred Contact:</h4><p> {dataPref} </p></div>
-          <div className={styles.datatext}><h4>Website type:</h4><p> {dataType} </p></div>
-          <div className={styles.datatext}><h4>Industry:</h4><p> {dataIndustry} </p></div>
-          <div className={styles.datatext}><h4>Additional comments:</h4><p> {dataAdditional} </p></div>
-          <div style={{marginTop: "4vh", width: "100%", display: "flex", alignItems: "center", justifyContent: "center"}}><Button onClick={() => { ;setActiveStep(4)}} className={styles.next} variant="outlined">Submit</Button></div>
+          <div className={styles.datatext}><h4>Name:</h4><p className={styles.datatextinfo}> {dataName} </p></div>
+          <div className={styles.datatext}><h4>Number:</h4><p className={styles.datatextinfo}> {dataNumber} </p></div>
+          <div className={styles.datatext}><h4>Email:</h4><p className={styles.datatextinfo}> {dataEmail} </p></div>
+          <div className={styles.datatext}><h4>Preferred Contact:</h4><p className={styles.datatextinfo}> {dataPref} </p></div>
+          <div className={styles.datatext}><h4>Website type:</h4><p className={styles.datatextinfo}> {dataType} </p></div>
+          <div className={styles.datatext}><h4>Industry:</h4><p className={styles.datatextinfo}> {dataIndustry} </p></div>
+          <div className={styles.datatext}><h4>Additional comments:</h4><p className={styles.datatextinfo}> {dataAdditional} </p></div>
+          <div style={{marginTop: "4vh", width: "100%", display: "flex", alignItems: "center", justifyContent: "center"}}><Button onClick={() => {submitData()}} className={styles.next} variant="outlined">Submit</Button></div>
         </div>
       </div>}
       {activeStep == 4 && <div className={styles.centercontainer}>
@@ -133,16 +147,18 @@ const BookProgress = ({progress}: {progress: number}) => {
           <Stepper activeStep={activeStep}>
             {steps.map((label, index) => {
               return (
-                <Step key={label} >
+                <Step key={index} >
                   <StepLabel>{label}</StepLabel>
                 </Step>
               );
             })}
           </Stepper>
         </div>
-        <h1 className={styles.primarytext}>Submission recieved!</h1>
-
-
+        <div className={styles.submissioncontainer}>
+          <CheckCircleRoundedIcon className={styles.submissionicon}/>
+          <h1 className={styles.submissionheading}>Submission recieved!</h1>
+          <h3 className={styles.submissiontext}> Thank you for your request, you will recieve a message soon via your preffered contact method.</h3>
+        </div>
       </div>}
     </div>
   );
--- pages/api/emailnoti.ts
+++ pages/api/emailnoti.ts
@@ -6,50 +6,52 @@ type Data = {
 
 var AWS = require("aws-sdk");
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-const SES_CONFIG = {
-  accessKeyId: process.env.SESUSER,
-  secretAccessKey: process.env.SESPASS,
-  region: 'us-west-2',
-};
+  if(!req.query.name || !req.query.number || !req.query.email || !req.query.pref || !req.query.plan || !req.query.type || !req.query.industry || !req.query.name ){res.status(400).json({ response: "Invalid Request" });}
 
-let params = {
-  Source: 'WebService@newt9.dev',
-  Destination: {
-    ToAddresses: [
-      'frankieiii15@comcast.net'
-    ],
-  },
-  ReplyToAddresses: [],
-  Message: {
-    Body: {
-      Html: {
-        Charset: 'UTF-8',
-        Data: 'Hello Newt, <br /> You have recieved a new request. <br /><br /> Account: Newt <br /> Data: True <br /> Details: Present',
-      },
+  const SES_CONFIG = {
+    accessKeyId: process.env.SESUSER,
+    secretAccessKey: process.env.SESPASS,
+    region: 'us-west-2',
+  };
+
+  let params = {
+    Source: 'WebService@newt9.dev',
+    Destination: {
+      ToAddresses: [
+        'frankieiii15@comcast.net'
+      ],
     },
-    Subject: {
-      Charset: 'UTF-8',
-      Data: `New Request`,
-    }
-  },
-};
+    ReplyToAddresses: [],
+    Message: {
+      Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: `Hello Newt, <br /> You have recieved a new request. <br /><br /> Client: ${req.query.name} <br /> Number: ${req.query.number} <br /> Email: ${req.query.email} <br /> Contact Pref: ${req.query.pref} <br /> Plan: ${req.query.plan} <br /> Website type: ${req.query.type} <br /> Industry: ${req.query.industry} <br /> Additional comments: ${req.query.additional}`
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: `New Request`,
+      }
+    },
+  };
 
-// Create the promise and SES service object
-var sendPromise = new AWS.SES(SES_CONFIG)
-  .sendEmail(params)
-  .promise();
+  // Create the promise and SES service object
+  var sendPromise = new AWS.SES(SES_CONFIG)
+    .sendEmail(params)
+    .promise();
 
-// Handle promise's fulfilled/rejected states
-sendPromise.then(function (data:any) {
-    console.log(data.MessageId);
-    res.status(200).json({ response: "Success"});
-  })
-  .catch(function (err:any) {
-    console.error(err, err.stack);
-    res.status(400).json({ response: "Error" });
-  });
+  // Handle promise's fulfilled/rejected states
+  return sendPromise.then(function (data:any) {
+      console.log(data.MessageId);
+      res.status(200).json({ response: "Success"});
+    })
+    .catch(function (err:any) {
+      console.error(err, err.stack);
+      res.status(400).json({ response: "Error" });
+    });
 }
